eed48b7bc053fde413f77aeccbecaf6a07b0f37f
mowslasher83 <radeswaled@hotmail.com>
2025-08-31 10:03:11 -0500

    feat: initial commit for App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chào mừng đến với Hệ Thống Thiết Kế Việt Nam</h1>
+    </div>
+  );
+}
+
+export default App;
