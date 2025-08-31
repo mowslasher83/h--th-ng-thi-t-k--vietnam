f2fbadf32906be3f0bd835e672a7be529c888abe
mowslasher83 <radeswaled@hotmail.com>
2025-08-31 10:05:10 -0500

    Add custom colors to Tailwind CSS theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: '#1DA1F2',
+        customGreen: '#00BF6F',
+      },
+    },
   },
   plugins: [],
 };
